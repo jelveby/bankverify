2d98cf70963f7ae5528b57c5ccb3f13a04f13410
Mikael Jelveby <mikael.jelveby@gmail.com>
2016-05-25 12:43:30 +0200

    Setting up environment and tasks

---
+++ gulpfile.babel.js
@@ -0,0 +1,6 @@
+import './tasks/build-spec.js';
+import './tasks/build.js';
+
+
+import './tasks/test-es6.js';
+import './tasks/test.js';
---
+++ tasks/build.js
@@ -0,0 +1,10 @@
+import gulp from "gulp";
+import babel from "gulp-babel";
+
+import paths from "../paths.json";
+
+gulp.task("build", () => {
+	return gulp.src(paths.source.javascript)
+		.pipe(babel())
+		.pipe(gulp.dest(paths.build.directories.output));
+});
