1db0ad777af9457504368d2d706bd8a5cf387aa3
Mikael Jelveby <mikael.jelveby@gmail.com>
2016-05-26 10:59:10 +0200

    Account normalization spec

---
+++ source/spec/account.normalize.spec.js
@@ -0,0 +1,34 @@
+import Account from '../lib/account.js';
+
+describe('Account - Normalize', () => {
+  let account;
+
+  beforeEach(() => {
+    account = Object.create(Account);
+  });
+
+  it('should normalize to clearing number dash serial number', () => {
+    account.init('11000000007');
+    account.normalize().should.eql('1100-0000007');
+  });
+
+  it('should keep any Swedbank/Sparbanker clearing checksum', () => {
+    account.init('800020000000000');
+    account.normalize().should.eql('8000-2-0000000000');
+  });
+
+  it('should not attempt to normalize invalid numbers', () => {
+    account.init(' 1-2-3 ');
+    account.normalize().should.eql(' 1-2-3 ');
+  });
+
+  it('should zero-fill serial numbers when needed', () => {
+    let no1 = Object.create(Account);
+    no1.init('8000-2-80000003');
+    let no2 = Object.create(Account);
+    no2.init('8000-2-8000000003');
+
+    no1.normalize().should.eql('8000-2-0080000003');
+    no2.normalize().should.eql('8000-2-8000000003');
+  });
+});
