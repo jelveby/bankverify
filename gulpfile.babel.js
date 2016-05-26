8e7c3f9551364bf95587dcaae61bd4f509a44576
Mikael Jelveby <mikael.jelveby@gmail.com>
2016-05-26 10:56:47 +0200

    Test coverage task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,6 @@
 import './tasks/build-spec.js';
 import './tasks/build.js';
 
-
 import './tasks/test-es6.js';
 import './tasks/test.js';
+import './tasks/test-coverage.js';
---
+++ tasks/test-coverage.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-process-exit */
+import gulp from 'gulp';
+import mocha from 'gulp-mocha';
+import istanbul from 'gulp-babel-istanbul';
+import paths from '../paths.json';
+
+import chai from 'chai';
+chai.should(); // This enables should-style syntax
+
+gulp.task('test-coverage', ['build'], callback => {
+	gulp.src(paths.source.javascript)
+		.pipe(istanbul()) // Covering files
+		.pipe(istanbul.hookRequire()) // Force `require` to return covered files
+		.on('finish', () => {
+			gulp.src(paths.source.spec)
+				.pipe(mocha())
+				.pipe(istanbul.writeReports({dir: `${__dirname}/../`, reporters: ['html', 'text']})) // Creating the reports after tests ran
+				// .pipe(istanbul.enforceThresholds({ thresholds: { global: 100 } })) // Enforce a coverage of 100%
+				.once('end', error => {
+					callback(error);
+					// Required to end the test due to
+					// interactive CLI testing
+					process.exit(0);
+				});
+		});
+});
