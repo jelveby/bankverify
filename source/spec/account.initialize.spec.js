61a82b4af24f51738e00c01acb803086d744ad62
Mikael Jelveby <mikael.jelveby@gmail.com>
2016-05-26 10:58:16 +0200

    Account initialization spec

---
+++ source/spec/account.initialize.spec.js
@@ -0,0 +1,31 @@
+import Account from '../lib/account.js';
+
+describe('Account - Initialize', () => {
+  let account;
+
+  beforeEach(() => {
+    account = Object.create(Account);
+  });
+
+  it('should initialize', () => {
+    account = Object.create(Account);
+
+    account.should.satisfy((acc) => {
+      return Object.getPrototypeOf(acc) === Account;
+    });
+  });
+
+  it('should set accountNumber', () => {
+    let accountNumber = '3000-0000000000';
+    account.init(accountNumber);
+
+    account.getAccountNumber().should.equal(accountNumber);
+  });
+
+  it('should return account number in only digits', () => {
+    account.init('3000-0000000000');
+
+    account.digits().should.equal('30000000000000');
+  });
+
+});
