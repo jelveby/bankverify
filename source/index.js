dad62d54401eac6493981603ae9d81d135d9c49b
Mikael Jelveby <mikael.jelveby@gmail.com>
2016-05-27 14:14:05 +0200

    Added Plusgiro to entry JavaScript file

--- source/index.js
+++ source/index.js
@@ -1,7 +1,9 @@
 import Account from './lib/account.js';
 import Bankgiro from './lib/bankgiro.js';
+import Plusgiro from './lib/plusgiro.js';
 
 export {
   Account,
-  Bankgiro
+  Bankgiro,
+  Plusgiro
 };
