aa827c7329e63485d9043d673fa4d2a9fb76ccdd
Mikael Jelveby <mikael.jelveby@gmail.com>
2016-05-27 12:57:15 +0200

    Bankgiro validation code

---
+++ source/lib/bankgiro.js
@@ -0,0 +1,57 @@
+import Utils from './utils.js';
+
+const Bankgiro = {
+  number: undefined,
+  init(number) {
+    this.number = number;
+    return this.number;
+  },
+  valid() {
+    return !this.errors().length;
+  },
+  errors() {
+    let errors = [];
+
+    if (this.digits().length < 7) {
+      errors.push(this.ERRORS.TOO_SHORT);
+    }
+
+    if (this.digits().length > 8) {
+      errors.push(this.ERRORS.TOO_LONG);
+    }
+
+    if (this.getNumber().match(/[^\d -]/)) {
+      errors.push(this.ERRORS.INVALID_CHARACTERS);
+    }
+
+    if (!Utils.validLuhn(this.digits())) {
+      errors.push(this.ERRORS.BAD_CHECKSUM);
+    }
+
+    return errors;
+  },
+  normalize() {
+    if (this.valid()) {
+      return this.digits().split(/(\d{4})$/).slice(0, 2).join('-');
+    } else {
+      return this.getNumber();
+    }
+  },
+  fundraising() {
+    return this.valid() && this.digits().search(/^90[0-4]/) !== -1;
+  },
+  getNumber() {
+    return this.number || '';
+  },
+  digits() {
+    return this.getNumber().replace(/\D/g, '');
+  },
+  ERRORS: Object.freeze({
+    TOO_SHORT: ':too_short',
+    TOO_LONG: ':too_long',
+    INVALID_CHARACTERS: ':invalid_characters',
+    BAD_CHECKSUM: ':bad_checksum'
+  })
+};
+
+export default Bankgiro;
