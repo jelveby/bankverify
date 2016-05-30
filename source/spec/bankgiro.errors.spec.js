19c2857635b62349d19569b84b4b23fd5e825119
Mikael Jelveby <mikael.jelveby@gmail.com>
2016-05-30 10:03:05 +0200

    Test spec cleanup

--- source/spec/bankgiro.errors.spec.js
+++ source/spec/bankgiro.errors.spec.js
@@ -18,15 +18,10 @@ describe('Bankgiro - Errors', () => {
   });
 
   it('should include TOO_SHORT for numbers shorter than 7 digits', () => {
-    let giro1 = Object.create(Bankgiro);
-    let giro2 = Object.create(Bankgiro);
-    let giro3 = Object.create(Bankgiro);
-    let giro4 = Object.create(Bankgiro);
-
-    giro1.init(undefined).errors().should.include(Bankgiro.ERRORS.TOO_SHORT);
-    giro2.init('').errors().should.include(Bankgiro.ERRORS.TOO_SHORT);
-    giro3.init('23-1332').errors().should.include(Bankgiro.ERRORS.TOO_SHORT);
-    giro4.init('92---------8838').errors().should.include(Bankgiro.ERRORS.TOO_SHORT);
+    bankgiro.init(undefined).errors().should.include(Bankgiro.ERRORS.TOO_SHORT);
+    bankgiro.init('').errors().should.include(Bankgiro.ERRORS.TOO_SHORT);
+    bankgiro.init('23-1332').errors().should.include(Bankgiro.ERRORS.TOO_SHORT);
+    bankgiro.init('92---------8838').errors().should.include(Bankgiro.ERRORS.TOO_SHORT);
   });
 
   it('should include TOO_LONG for numbers longer than 8 digits', () => {
@@ -34,11 +29,8 @@ describe('Bankgiro - Errors', () => {
   });
 
   it('should include INVALID_CHARACTERS for numbers with other character than digits, spaces and dashes', () => {
-    let giro1 = Object.create(Bankgiro);
-    giro1.init('5402-9681X').errors().should.include(Bankgiro.ERRORS.INVALID_CHARACTERS);
-
-    let giro2 = Object.create(Bankgiro);
-    giro2.init(' 5 4 0 2 - 9 6 8 1 ').errors().should.not.include(Bankgiro.ERRORS.INVALID_CHARACTERS);
+    bankgiro.init('5402-9681X').errors().should.include(Bankgiro.ERRORS.INVALID_CHARACTERS);
+    bankgiro.init(' 5 4 0 2 - 9 6 8 1 ').errors().should.not.include(Bankgiro.ERRORS.INVALID_CHARACTERS);
   });
 
   it('should include BAD_CHECKSUM if the Luhn/mod 10 checksum is incorrect', () => {
--- source/spec/plusgiro.error.spec.js
+++ source/spec/plusgiro.error.spec.js
@@ -18,15 +18,10 @@ describe('Plusgiro - Errors', () => {
   });
 
   it('should include TOO_SHORT for numbers shorter than 2 digits', () => {
-    let plusgiro1 = Object.create(Plusgiro);
-    let plusgiro2 = Object.create(Plusgiro);
-    let plusgiro3 = Object.create(Plusgiro);
-    let plusgiro4 = Object.create(Plusgiro);
-
-    plusgiro1.init(null).errors().should.include(Plusgiro.ERRORS.TOO_SHORT);
-    plusgiro2.init('').errors().should.include(Plusgiro.ERRORS.TOO_SHORT);
-    plusgiro3.init('1').errors().should.include(Plusgiro.ERRORS.TOO_SHORT);
-    plusgiro4.init('1-------').errors().should.include(Plusgiro.ERRORS.TOO_SHORT);
+    plusgiro.init(null).errors().should.include(Plusgiro.ERRORS.TOO_SHORT);
+    plusgiro.init('').errors().should.include(Plusgiro.ERRORS.TOO_SHORT);
+    plusgiro.init('1').errors().should.include(Plusgiro.ERRORS.TOO_SHORT);
+    plusgiro.init('1-------').errors().should.include(Plusgiro.ERRORS.TOO_SHORT);
   });
 
   it('should include TOO_LONG for number longer than 8 digits', () => {
@@ -34,11 +29,8 @@ describe('Plusgiro - Errors', () => {
   });
 
   it('should include INVALID_CHARACTERS for numbers with other character than digits, spaces and dashes', () => {
-    let plusgiro1 = Object.create(Plusgiro);
-    plusgiro1.init('410 54 68-5X').errors().should.include(Plusgiro.ERRORS.INVALID_CHARACTERS);
-
-    let plusgiro2 = Object.create(Plusgiro);
-    plusgiro2.init('4 1 0 5 4 6 8 - 5 ').errors().should.not.include(Plusgiro.ERRORS.INVALID_CHARACTERS);
+    plusgiro.init('410 54 68-5X').errors().should.include(Plusgiro.ERRORS.INVALID_CHARACTERS);
+    plusgiro.init('4 1 0 5 4 6 8 - 5 ').errors().should.not.include(Plusgiro.ERRORS.INVALID_CHARACTERS);
   });
 
   it('should include BAD_CHECKSUM if the Luhn/mod 10 checksum is incorrect', () => {
