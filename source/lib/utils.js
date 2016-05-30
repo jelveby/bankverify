be927faab928aa93f582d0d8bdbf25513112bd8f
Mikael Jelveby <mikael.jelveby@gmail.com>
2016-05-30 13:45:34 +0200

    mod11 algorithm

--- source/lib/utils.js
+++ source/lib/utils.js
@@ -1,5 +1,5 @@
 const Utils = {
-  mod10: function (value) {
+  mod10(value) {
     if (/[^0-9-\s]+/.test(value)) { return false; }
 
     let len = value.length,
@@ -13,7 +13,31 @@ const Utils = {
         sum += (bit ^= 1) ? arr[val] : val;
     }
 
-    return sum && sum % 10 === 0;
+    return sum % 10 === 0;
+  },
+  mod11(value) {
+    let result = false,
+        weight = [1, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
+        controlNumber = parseInt(value.slice(-1)),
+        base = value.slice(0, -1);
+
+    let total = base.split('').reduce((sum, digit, idx) => {
+      return sum + (parseInt(digit, 10) * weight[idx]);
+    }, 0);
+    let remainder = total % 11;
+
+    switch (remainder) {
+      case 0:
+        result = controlNumber === 0;
+      break;
+      case 1:
+        result = false;
+      break;
+      default:
+        result = controlNumber === (11 - remainder);
+      break;
+    }
+    return result;
   }
 };
 
