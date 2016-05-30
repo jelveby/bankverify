4da57e81b18720b7dc7ac4b874d3ca3a2a563366
Mikael Jelveby <mikael.jelveby@gmail.com>
2016-05-30 13:33:51 +0200

    Utils test spec

---
+++ source/spec/utils.spec.js
@@ -0,0 +1,58 @@
+import Utils from '../lib/utils.js';
+import { expect } from 'chai';
+
+describe('Utils', () => {
+  beforeEach(() => {
+  });
+
+  describe('mod10', () => {
+    [
+      '00',
+      '18',
+      '26',
+      '34',
+      '42',
+      '59',
+      '67',
+      '75',
+      '83',
+      '91',
+      '109',
+      '117',
+      '54029681'
+    ].forEach(number => {
+      it(`should allow valid numbers like ${number}`, () => {
+        expect(Utils.mod10(number)).to.be.true;
+      });
+    });
+
+    [
+      '01',
+      '118',
+      '54029682'
+    ].forEach(number => {
+      it(`should not allow invalid numbers like ${number}`, () => {
+        expect(Utils.mod10(number)).to.be.false;
+      });
+    });
+  });
+
+  describe('mod11', () => {
+    [
+      '71591293050'
+    ].forEach(number => {
+      it(`should allow valid numbers like ${number}`, () => {
+        expect(Utils.mod11(number)).to.be.true;
+      });
+    });
+
+    [
+      '81591293050'
+    ].forEach(number => {
+      it(`should not allow invalid numbers like ${number}`, () => {
+        expect(Utils.mod11(number)).to.be.false;
+      });
+    });
+  });
+
+});
