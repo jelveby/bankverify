a70d50288b7108e0db6b6c6bc794a008b7254f0c
Mikael Jelveby <mikael.jelveby@gmail.com>
2016-06-01 11:17:28 +0200

    Added account types

---
+++ source/lib/account/accountType.js
@@ -0,0 +1,9 @@
+const AccountType = {
+  TYPE1: { algorithm: 'mod11', clearingLength: 4, serialLength: 7, weightedNumbers: 10 },
+  TYPE2: { algorithm: 'mod11', clearingLength: 4, serialLength: 7, weightedNumbers: 11 },
+  TYPE3: { algorithm: 'mod10', clearingLength: 4, serialLength: 10, weightedNumbers: 10 },
+  TYPE4: { algorithm: 'mod10', clearingLength: 4, serialLength: 9, weightedNumbers: 9 },
+  TYPE5: { algorithm: 'mod10', clearingLength: 5, serialLength: 10, weightedNumbers: 10 }
+};
+
+export default AccountType;
