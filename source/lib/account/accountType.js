7fb9910d19996bf5902c6034d6e6daf32d1ec836
Mikael Jelveby <mikael.jelveby@gmail.com>
2016-06-01 16:07:41 +0200

    Wrong algorithm for TYPE4

--- source/lib/account/accountType.js
+++ source/lib/account/accountType.js
@@ -4,7 +4,7 @@ const AccountType = {
   TYPE1: { algorithm: 'mod11', clearingLength: 4, serialLength: 7, weightedNumbers: 10 },
   TYPE2: { algorithm: 'mod11', clearingLength: 4, serialLength: 7, weightedNumbers: 11 },
   TYPE3: { algorithm: 'mod10', clearingLength: 4, serialLength: 10, weightedNumbers: 10 },
-  TYPE4: { algorithm: 'mod10', clearingLength: 4, serialLength: 9, weightedNumbers: 9 },
+  TYPE4: { algorithm: 'mod11', clearingLength: 4, serialLength: 9, weightedNumbers: 9 },
   TYPE5: { algorithm: 'mod10', clearingLength: 5, serialLength: 10, weightedNumbers: 10 }
 };
 
