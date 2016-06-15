cae431ffe0070c4ad7d0f76e2c395f34373f5da0
Mikael Jelveby <mikael.jelveby@gmail.com>
2016-06-15 16:19:31 +0200

    Object.create() polyfill

--- source/index.js
+++ source/index.js
@@ -2,6 +2,26 @@ import Account from './lib/account.js';
 import Bankgiro from './lib/bankgiro.js';
 import Plusgiro from './lib/plusgiro.js';
 
+if (typeof Object.create !== 'function') {
+	Object.create = (function() {
+		var Temp = function() {};
+		return function (prototype) {
+			if (arguments.length > 1) {
+				throw Error('Second argument not supported');
+			}
+			if (prototype !== Object(prototype) && prototype !== null) {
+				throw new TypeError('Argument must be an object or null');
+			}
+			if (prototype === null) {
+				throw Error('null [[Prototype]] not supported');
+			}
+			Temp.prototype = prototype;
+			var result = new Temp();
+			Temp.prototype = null;
+			return result;
+		};
+	})();
+}
 export {
   Account,
   Bankgiro,
